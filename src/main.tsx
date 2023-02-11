828274883867d2d38687e37f3e6293060dc03ea7
Izaías Lima <izaiaslima356@gmail.com>
2023-02-10 22:17:46 -0300

    chore: set up react-query, axios and json-server

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "./styles/theme";
+import { QueryClientProvider } from "react-query";
+import { queryClient } from "./services/react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
 		<ChakraProvider theme={theme}>
-			<App />
+			<QueryClientProvider client={queryClient}>
+				<App />
+			</QueryClientProvider>
 		</ChakraProvider>
 	</React.StrictMode>
 );
--- src/store/useCoupon.ts
+++ src/store/useCoupon.ts
@@ -1,6 +1,6 @@
 import { create } from "zustand";
 
-interface Coupon {
+interface ICoupon {
 	couponCode: string;
 	discountType: "percentual" | "fixed";
 	discountValue: number;
@@ -9,30 +9,25 @@ interface Coupon {
 	amountByClient: number;
 	isActive: boolean;
 	firstPurchase: boolean;
-	availibility: [
-		startDay: number,
-		endDay: number,
-		startDate: Date,
-		endDate: Date,
-		startHour: number,
-		endHour: number
-	];
 }
 
-interface CouponStore {
-	couponCode: string;
-	discountType: "percentual" | "fixed";
-	discountValue: number;
-	totalAmount: number;
-	totalAvailable: number;
-	isActive: boolean;
+// availibility: [
+// 	startDay: number,
+// 	endDay: number,
+// 	startDate: Date,
+// 	endDate: Date,
+// 	startHour: number,
+// 	endHour: number
+// ];
+
+interface ICouponStore {
+	couponList: ICoupon[];
+	setCouponList: (couponList: ICoupon[]) => void;
 }
 
-export const useCoupon = create<CouponStore>((set) => ({
-	couponCode: "",
-	discountType: "fixed",
-	discountValue: 0,
-	totalAmount: 0,
-	totalAvailable: 0,
-	isActive: true,
+export const useCoupon = create<ICouponStore>((set) => ({
+	couponList: [],
+	setCouponList(couponList: ICoupon[]) {
+		set(() => ({ couponList }));
+	},
 }));
